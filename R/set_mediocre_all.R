f67a1cfc3317da60133bebf542aba868072c8816
Vincent Bagilet <vincent.bagilet@columbia.edu>
2020-09-16 16:09:29 +0200

    Simplify scale functions
    Use scale_mediocre_d instead of scale_fill_mediocre_d, scale_color_mediocre_d and scale_colour_mediocre_d

--- R/set_mediocre_all.R
+++ R/set_mediocre_all.R
@@ -6,8 +6,8 @@
 #'
 set_document_mediocre <- function() {
   ggplot2::theme_set(mediocrethemes::theme_mediocre())
-  options(ggplot2.continuous.fill = mediocrethemes::scale_fill_mediocre_c)
-  options(ggplot2.continuous.colour = mediocrethemes::scale_color_mediocre_c)
-  options(ggplot2.discrete.fill = mediocrethemes::scale_fill_mediocre_d)
-  options(ggplot2.discrete.colour = mediocrethemes::scale_color_mediocre_d)
+  options(ggplot2.continuous.fill = mediocrethemes::scale_mediocre_c)
+  options(ggplot2.continuous.colour = mediocrethemes::scale_mediocre_c)
+  options(ggplot2.discrete.fill = mediocrethemes::scale_mediocre_d)
+  options(ggplot2.discrete.colour = mediocrethemes::scale_mediocre_d)
 }
