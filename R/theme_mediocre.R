9c4b297dd72b0cfa01266b710dbb9361671a7a95
Vincent Bagilet <vincent.bagilet@columbia.edu>
2020-09-16 16:26:06 +0200

    Using rel()

--- R/theme_mediocre.R
+++ R/theme_mediocre.R
@@ -33,20 +33,20 @@ theme_mediocre <- function() {
   ggplot2::theme_minimal() %+replace%
     ggplot2::theme(
       plot.background = ggplot2::element_rect(fill = color_type[["background"]]),
-      panel.grid.major.y = ggplot2::element_line(colour = color_type[["dark"]], size = 0.1),
+      panel.grid.major.y = ggplot2::element_line(colour = color_type[["dark"]], size = rel(0.1)),
       panel.grid.minor.x = ggplot2::element_blank(),
       panel.grid.major.x =  ggplot2::element_blank(),
       panel.grid.minor.y = ggplot2::element_blank(),
       text = ggplot2::element_text(family = "nunito", colour = color_type[["dark"]],),
-      axis.text = ggplot2::element_text(size = 10),
+      axis.text = ggplot2::element_text(size = rel(0.9)),
       axis.text.x = ggplot2::element_text(angle = 45),
-      axis.ticks.x = ggplot2::element_line(size = 0.2),
+      axis.ticks.x = ggplot2::element_line(size = rel(0.2)),
       plot.title.position = "plot",
-      plot.title = ggplot2::element_text(hjust = 0.1, size = 13),
-      plot.subtitle = ggplot2::element_text(hjust = 0.12, vjust = 0, size = 10, face = "italic"),
+      plot.title = ggplot2::element_text(hjust = rel(0.1), size = rel(1.2)),
+      plot.subtitle = ggplot2::element_text(hjust = rel(0.12), vjust = rel(0), size = rel(0.9), face = "italic"),
       legend.position = "top",
       legend.title.align = 0.5,
-      legend.title = ggplot2::element_text(size = 10, vjust = 0.8),
-      legend.text = ggplot2::element_text(size = 8)
+      legend.title = ggplot2::element_text(size = rel(0.9)),
+      legend.text = ggplot2::element_text(size = rel(0.8))
     )
 }
